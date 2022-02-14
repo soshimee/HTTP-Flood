f14b583cac7ab3d0d407f7d5fc9ad8b4fcc39edc
soshimee <kimyejung418@gmail.com>
2022-02-14 13:25:54 +0900

    fix

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ import fs from "fs";
 import fetch from "node-fetch";
 
 const config = JSON.parse(fs.readFileSync("config.json"));
-let conn = 0;
 
 for (let i = 0; i < config.threads; i++) {
 	setInterval(async () => {
-		try {
-			await fetch(config.url);
-		} catch {}
-		console.log(++conn);
+		fetch(config.url).catch(() => {});
 	}, 0);
 }
